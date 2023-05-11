8b3eda0bb324f8c7dd881af23cf8e25d114bcee5
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2023-05-11 04:31:16 +0400

    created models

--- Core/admin.py
+++ Core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import WebsiteSetting, UserProfile, MetaInfo
 # Register your models here.
+
+admin.site.register(WebsiteSetting)
+admin.site.register(UserProfile)
+admin.site.register(MetaInfo)
--- Core/models.py
+++ Core/models.py
@@ -7,6 +7,10 @@ class WebsiteSetting(models.Model):
     name = models.CharField(max_length=50)
     value = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = 'Website Setting'
+        verbose_name_plural = 'Website Settings'
+
     def __str__(self):
         return self.name
 
@@ -14,6 +18,10 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
     profile_pic = models.ImageField(upload_to='profile_pics/')
+
+    class Meta:
+        verbose_name = 'User Profile'
+        verbose_name_plural = 'User Profiles'
 
     def __str__(self):
         return self.user.username
@@ -24,5 +32,8 @@ class MetaInfo(models.Model):
     description = models.TextField()
     keywords = models.TextField()
 
+    class Meta:
+        verbose_name = 'Meta Info'
+        verbose_name_plural = 'Meta Infos'
     def __str__(self):
         return self.page_name
