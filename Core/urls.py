42c9f3f33e53351a3e5f1ea7214b5b3ef04e42bc
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2023-05-22 01:44:10 +0400

    added cart in url.py

--- Core/urls.py
+++ Core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import home
+from .views import home, cart
 
 urlpatterns = [
     path("home/", home, name="home"),
+    path("cart/", cart, name="cart")
 ]
