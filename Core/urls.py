f16bc7680fd4c3de4e1fdabfa11bfe8c3f2fbb3a
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2023-05-23 22:21:19 +0400

    settings path added

--- Core/urls.py
+++ Core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import home, cart
+from .views import home, cart,settings
 
 urlpatterns = [
     path("home/", home, name="home"),
-    path("cart/", cart, name="cart")
+    path("cart/", cart, name="cart"),
+    path("settings/", settings, name="settings"),
 ]
