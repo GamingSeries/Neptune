154d30cd6c0817114495affde15425a845d64a8e
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2023-06-13 16:40:58 +0400

    context

--- Core/views.py
+++ Core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import UserProfile
 # Create your views here.
 
 def home(request):
-    return render(request, "core/home.html")
+    context = {}
+    context['userprofile'] = UserProfile.objects.all()
+    return render(request,  "core/home.html", context)
 
 def cart(request):
     return render(request, "core/cart.html")
