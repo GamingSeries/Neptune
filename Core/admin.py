1529e1dae98295d21168eb4cb63d9e5eafa1515e
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2023-06-17 01:16:59 +0400

    UserLogin added

--- Core/admin.py
+++ Core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import WebsiteSetting, UserProfile, MetaInfo
+from .models import WebsiteSetting, UserProfile, MetaInfo, UserLogin
 # Register your models here.
 
 admin.site.register(WebsiteSetting)
 admin.site.register(UserProfile)
 admin.site.register(MetaInfo)
+admin.site.register(UserLogin)
