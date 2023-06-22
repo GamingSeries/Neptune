c513707c637537237aabdcb628d81bc70ef047f5
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2023-06-22 19:40:39 +0400

    Login form frontend created

--- neptunereact/src/App.js
+++ neptunereact/src/App.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Login';  // Ensure the path is correct based on your file structure
+import Login from './components/Login';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        {/* Add more routes as necessary */}
-      </Routes>
-    </Router>
+    <div className="App">
+      <Login />
+    </div>
   );
 }
 
