aba4cafe84e599e980f54c77bfd43bc4e83044cf
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2024-03-07 17:54:12 -0500

    cleared the mess

--- backend/dashboard/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import DashboardView
-
-urlpatterns = [
-    path('dashboard/<str:user_type>/', DashboardView.as_view(), name='dashboard'),
-]
--- backend/neptune/settings.py
+++ backend/neptune/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 
     #apps
     'user',
-    'dashboard',
+    'home',
     
 ]
 
