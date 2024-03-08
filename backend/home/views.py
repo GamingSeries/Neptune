8b07d4d02b2e5472a231af088278979a752b9f78
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2024-03-08 00:53:11 -0500

    creaed the registration logic

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
 # Create your views here.
+
+
 def home(request):
     return render(request, './html/index.html')
 
 def login(request):
-    #create login handler
-    
     return render(request, './html/login.html')
+
+
+#registering user 
+def register_user(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('registration_success')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
