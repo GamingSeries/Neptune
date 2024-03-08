acd6ca496d63676f4bf97bfe60dc6de4913a90df
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2024-03-07 22:27:59 -0500

    added django hashing library

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 class User(models.Model):
@@ -9,6 +10,13 @@ class User(models.Model):
     address = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=10)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    #hashing password with django hashing library
+    def save(self, *args, **kwargs):
+        # Hash the password before saving
+        self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.username
     
