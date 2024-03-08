e96fcffc92744d0f6786710fda2d8fab14efd255
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2024-03-07 21:01:41 -0500

    created 'User' model

--- backend/home/views.py
+++ backend/home/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, './html/index.html')
+
+def login(request):
+    #create login handler
+    
+    return render(request, './html/login.html')
